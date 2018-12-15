5858f85b6d773b2f91ed0d716a81ad60c72d17b8
andygrie <andy.grieszer@gmx.at>
2018-12-15 22:53:48 +0100

    fix selectionChange bug

--- src/app/components/new-scrobble.dialog/new-scrobble.dialog.component.spec.ts
+++ src/app/components/new-scrobble.dialog/new-scrobble.dialog.component.spec.ts
@@ -1,25 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { NewScrobble.DialogComponent } from './new-scrobble.dialog.component';
+import { ConfirmDialogNewScrobble } from "./new-scrobble.dialog.component";
 
-describe('NewScrobble.DialogComponent', () => {
-  let component: NewScrobble.DialogComponent;
-  let fixture: ComponentFixture<NewScrobble.DialogComponent>;
+describe("ConfirmDialogNewScrobble", () => {
+  let component: ConfirmDialogNewScrobble;
+  let fixture: ComponentFixture<ConfirmDialogNewScrobble>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewScrobble.DialogComponent ]
-    })
-    .compileComponents();
+      declarations: [ConfirmDialogNewScrobble]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NewScrobble.DialogComponent);
+    fixture = TestBed.createComponent(ConfirmDialogNewScrobble);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
